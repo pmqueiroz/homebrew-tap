916a04220e7860ce02698600900e9b76bbefd70e
pmqueiroz <pedroqueiroz593@gmail.com>
2024-06-21 23:07:50 -0300

    fix: update sha to use 256

--- Formula/import-holmes.rb
+++ Formula/import-holmes.rb
@@ -2,7 +2,7 @@ class ImportHolmes < Formula
   version "v0.1.0"
   desc "CLI tool to inspect Javascript/Typescript project imports"
   homepage "https://github.com/pmqueiroz/import-holmes"
-  sha256 "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+  sha256 "20bd13663ecd92fe13bac54292e2baca56631a17ab6e2536f518887fa93244a6"
   
   if OS.mac?
     url "#{homepage}/releases/download/#{version}/import-holmes-#{version}-x86_64-apple-darwin.tar.gz"
