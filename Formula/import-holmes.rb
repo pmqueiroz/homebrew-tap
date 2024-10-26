e487748b51f2f7744ebce70b622b99aae301e4f2
pmqueiroz <pedroqueiroz593@gmail.com>
2024-10-25 22:02:41 -0300

    feat(import-holmes): release v0.4.0

--- Formula/import-holmes.rb
+++ Formula/import-holmes.rb
@@ -1,14 +1,14 @@
 class ImportHolmes < Formula
-  version "v0.3.0"
+  version "v0.4.0"
   desc "CLI tool to inspect Javascript/Typescript project imports"
   homepage "https://github.com/pmqueiroz/import-holmes"
   
   if OS.mac?
     url "#{homepage}/releases/download/#{version}/import-holmes-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "0b55a0a8ad6c15bc4c25d066edf9ffc5e6a03da2e4b5c795d346293901735dfe"
+    sha256 "ac5a1c6f2f0dae04ac65c7ecddb0f774e6be44d4215824e6cdc17df2e6c71718"
   elsif OS.linux?
     url "#{homepage}/releases/download/#{version}/import-holmes-#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "5bb1c63ea19f49cfc4c3c7f04ef5674837afa6c5f7d07311392a4e988bb471ce"
+    sha256 "03f26631c6daf512250e3c86f5cd6c85f7d6292f4648a953cea67bdbf5b3e626"
   end
 
   def install
