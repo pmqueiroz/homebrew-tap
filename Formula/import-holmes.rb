cb718796cfc7ba85d3e0da42d629e378dde19114
pmqueiroz <pedroqueiroz593@gmail.com>
2025-02-05 12:47:02 -0300

    feat(import-holmes): release v0.5.1

--- Formula/import-holmes.rb
+++ Formula/import-holmes.rb
@@ -1,14 +1,14 @@
 class ImportHolmes < Formula
-  version "v0.5.0"
+  version "v0.5.1"
   desc "CLI tool to inspect project imports"
   homepage "https://github.com/pmqueiroz/import-holmes"
   
   if OS.mac?
     url "#{homepage}/releases/download/#{version}/import-holmes-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "2e802c36d8c815b5f22937b1922cabfce537f0f4a687624083e77d504dd17657"
+    sha256 "0d8686055e23793276d08b2e419ac600751b128e270ed519f65bbb1862d2ec30"
   elsif OS.linux?
     url "#{homepage}/releases/download/#{version}/import-holmes-#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "fb2b7bb6b525f37061313a0dba3319e5e5e66d1a7dff6712ab921a847260d584"
+    sha256 "f7580f7e8079274dcaa5e7e244d731b02c332dd9d54c71e766fdc3d6f30737b2"
   end
 
   def install
